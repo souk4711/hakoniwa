341a313ca53a2189c2a41ffd9d091c5d71ca38bb
John Doe <johndoe@example.com>
2022-08-20 00:59:52 +0800

    docs: Add docs for CLI

--- hakoniwa-cli/src/lib.rs
+++ hakoniwa-cli/src/lib.rs
@@ -1,3 +1,11 @@
+//! Process isolation for Linux using namespaces, resource limits and seccomp. It
+//! works by creating a new, completely empty, mount namespace where the root is
+//! on a tmpfs that is invisible from the host, and will be automatically cleaned
+//! up when the last process exits. You can then use a policy configuration file or
+//! commandline options to construct the root filesystem and process environment
+//! and command to run in the namespace.
+//!
+//! More information can be found in [homepage](https://github.com/souk4711/hakoniwa).
 mod cli;
 mod contrib;
 mod embed;
