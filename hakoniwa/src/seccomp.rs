902d209811e3c1887733285dd6bae0a632f064b2
John Doe <johndoe@example.com>
2022-08-20 16:14:14 +0800

    refactor: Add SeccompAction

--- hakoniwa/src/seccomp.rs
+++ hakoniwa/src/seccomp.rs
@@ -2,22 +2,44 @@ use libseccomp::ScmpAction;
 use serde::Deserialize;
 
 #[derive(Deserialize, Default, Debug)]
+pub enum SeccompAction {
+    #[default]
+    KillProcess,
+    Allow,
+}
+
+impl SeccompAction {
+    fn to_scmp_action(&self) -> ScmpAction {
+        match self {
+            Self::KillProcess => ScmpAction::KillProcess,
+            Self::Allow => ScmpAction::Allow,
+        }
+    }
+}
+
+#[derive(Deserialize, Default, Debug)]
 pub struct Seccomp {
     pub(crate) syscalls: Vec<String>,
+    #[serde(skip)]
+    pub(crate) dismatch_action: SeccompAction,
+    #[serde(skip)]
+    pub(crate) match_action: SeccompAction,
 }
 
 impl Seccomp {
     pub fn new() -> Self {
         Self {
+            dismatch_action: SeccompAction::KillProcess,
+            match_action: SeccompAction::Allow,
             ..Default::default()
         }
     }
 
     pub fn dismatch_action(&self) -> ScmpAction {
-        ScmpAction::KillProcess
+        self.dismatch_action.to_scmp_action()
     }
 
     pub fn match_action(&self) -> ScmpAction {
-        ScmpAction::Allow
+        self.match_action.to_scmp_action()
     }
 }
