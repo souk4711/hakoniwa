a4a2786067bff40b94d3e3b7cb1eb172a3a47a0d
John Doe <johndoe@example.com>
2025-04-11 15:53:33 +0800

    feat(cli): Add jinja::fs_glob

--- hakoniwa-cli/src/contrib/jinja/fs.rs
+++ hakoniwa-cli/src/contrib/jinja/fs.rs
@@ -14,8 +14,19 @@ pub(crate) fn is_symlink(value: String) -> bool {
     Path::new(&value).is_symlink()
 }
 
+pub(crate) fn glob(value: String) -> Result<Vec<String>, Error> {
+    glob::glob(&value)
+        .map_err(|e| Error::new(InvalidOperation, "cannot glob").with_source(e))
+        .map(|paths| {
+            paths
+                .filter_map(|path| path.ok())
+                .map(|pathbuf| pathbuf.to_string_lossy().to_string())
+                .collect()
+        })
+}
+
 pub(crate) fn read_link(value: String) -> Result<String, Error> {
     fs::read_link(&value)
         .map_err(|e| Error::new(InvalidOperation, "cannot read link").with_source(e))
-        .map(|v| v.as_path().to_string_lossy().to_string())
+        .map(|pathbuf| pathbuf.to_string_lossy().to_string())
 }
