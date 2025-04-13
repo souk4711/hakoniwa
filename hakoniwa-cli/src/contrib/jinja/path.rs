14b7bc45ee79f4948d2997ac70be5ebbf67cbb23
John Doe <johndoe@example.com>
2025-04-14 00:14:22 +0800

    fix: Fix path_exists

--- hakoniwa-cli/src/contrib/jinja/path.rs
+++ hakoniwa-cli/src/contrib/jinja/path.rs
@@ -1,7 +1,7 @@
 use std::path::Path;
 
 pub(crate) fn exists(value: String) -> bool {
-    Path::new(&value).try_exists().is_ok()
+    Path::new(&value).try_exists().is_ok_and(|v| v)
 }
 
 pub(crate) fn is_symlink(value: String) -> bool {
