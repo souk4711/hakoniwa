918efd69fc9859468353448e00d18fecf829ed49
souk4711 <53728155+souk4711@users.noreply.github.com>
2026-06-11 00:56:07 +0800

    refactor: Runctl, sort by alphanum

--- hakoniwa/src/runctl.rs
+++ hakoniwa/src/runctl.rs
@@ -16,20 +16,20 @@ pub enum Runctl {
     /// rejected configuration due to permissions.
     IgnoreCgroupSetupFailed,
 
-    /// Start a new session and acquire the controlling terminal before
-    /// executing the program.
-    ///
-    /// Just prior to `execve`, this calls `setsid(2)` so the process becomes
-    /// the leader of a new session and process group, then issues
-    /// `ioctl(stdin, TIOCSCTTY)` to make the terminal referenced by stdin the
-    /// controlling terminal of that session. This is typically what you want
-    /// when running an interactive shell so that job control works correctly.
-    NewSession,
-
     /// Fallback when the specific configuration is not applicable. E.g try to
     /// remount a bind mount again after the first attempt failed on source
     /// filesystems that have nodev, noexec, nosuid, etc.
     MountFallback,
+
+    /// Start a new session and acquire the controlling terminal before
+    /// executing the program.
+    ///
+    /// Just prior to `execve`, this calls `setsid(2)` so the internal process
+    /// becomes the leader of a new session and process group, then issues
+    /// `ioctl(stdin, TIOCSCTTY)` to make the terminal referenced by stdin the
+    /// controlling terminal of that session. This is typically what you want
+    /// when running an interactive shell so that job control works correctly.
+    NewSession,
 
     /// Mount root dir with read-write access.
     RootdirRW,
