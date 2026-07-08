a043d7d6d2eef48e7f8c29fc40b302daf9424107
souk4711 <53728155+souk4711@users.noreply.github.com>
2026-07-08 21:05:40 +0800

    test: add ptrace relative test cases

--- hakoniwa/tests/child_test.rs
+++ hakoniwa/tests/child_test.rs
@@ -2,120 +2,136 @@
 mod child_test {
     use std::{thread, time};
 
-    use hakoniwa::{Command, Container};
+    use hakoniwa::{Command, Container, Runctl};
 
     fn command(program: &str) -> Command {
         Container::new().rootfs("/").unwrap().command(program)
     }
 
+    fn command_as_tracee(program: &str) -> Command {
+        Container::new()
+            .runctl(Runctl::GetProcPidStatus)
+            .rootfs("/")
+            .unwrap()
+            .command(program)
+    }
+
     #[test]
     fn test_try_wait_terminated() {
-        let mut child = command("/bin/sleep").arg("1").spawn().unwrap();
+        for mut cmd in vec![command("/bin/sleep"), command_as_tracee("/bin/sleep")] {
+            let mut child = cmd.arg("1").spawn().unwrap();
 
-        let status = child.try_wait().unwrap();
-        assert!(status.is_none());
+            let status = child.try_wait().unwrap();
+            assert!(status.is_none());
 
-        thread::sleep(time::Duration::from_secs(2));
-        let status = child.try_wait().unwrap();
-        assert!(status.is_some());
-        assert!(status.unwrap().success());
+            thread::sleep(time::Duration::from_secs(2));
+            let status = child.try_wait().unwrap();
+            assert!(status.is_some());
+            assert!(status.unwrap().success());
+        }
     }
 
     #[test]
     fn test_try_wait_timeout() {
-        let mut child = command("/bin/sleep")
-            .arg("4")
-            .wait_timeout(1)
-            .spawn()
-            .unwrap();
+        for mut cmd in vec![command("/bin/sleep"), command_as_tracee("/bin/sleep")] {
+            let mut child = cmd.arg("4").wait_timeout(1).spawn().unwrap();
 
-        let status = child.try_wait().unwrap();
-        assert!(status.is_none());
+            let status = child.try_wait().unwrap();
+            assert!(status.is_none());
 
-        thread::sleep(time::Duration::from_secs(2));
-        let status = child.try_wait().unwrap().unwrap();
-        assert!(!status.success());
-        assert_eq!(status.code, 128 + 9);
-        assert_eq!(status.reason, "process(/bin/sleep) received signal SIGKILL");
-        assert_eq!(status.exit_code, None);
-        assert_eq!(status.rusage.unwrap().real_time.as_secs(), 1);
+            thread::sleep(time::Duration::from_secs(2));
+            let status = child.try_wait().unwrap().unwrap();
+            assert!(!status.success());
+            assert_eq!(status.code, 128 + 9);
+            assert_eq!(status.reason, "process(/bin/sleep) received signal SIGKILL");
+            assert_eq!(status.exit_code, None);
+            assert_eq!(status.rusage.unwrap().real_time.as_secs(), 1);
+        }
     }
 
     #[test]
     fn test_try_wait_killed() {
-        let mut child = command("/bin/sleep").arg("1").spawn().unwrap();
+        for mut cmd in vec![command("/bin/sleep"), command_as_tracee("/bin/sleep")] {
+            let mut child = cmd.arg("1").spawn().unwrap();
 
-        let status = child.try_wait().unwrap();
-        assert!(status.is_none());
+            let status = child.try_wait().unwrap();
+            assert!(status.is_none());
 
-        child.kill().unwrap();
-        thread::sleep(time::Duration::from_secs(2));
-        let status = child.try_wait().unwrap().unwrap();
-        assert!(!status.success());
-        assert_eq!(status.code, 125);
-        assert_eq!(status.reason, "container received signal SIGKILL");
-        assert_eq!(status.exit_code, None);
-        assert!(status.rusage.is_none());
+            child.kill().unwrap();
+            thread::sleep(time::Duration::from_secs(2));
+            let status = child.try_wait().unwrap().unwrap();
+            assert!(!status.success());
+            assert_eq!(status.code, 125);
+            assert_eq!(status.reason, "container received signal SIGKILL");
+            assert_eq!(status.exit_code, None);
+            assert!(status.rusage.is_none());
+        }
     }
 
     #[test]
     fn test_try_wait_then_wait() {
-        let mut child = command("/bin/sleep").arg("1").spawn().unwrap();
+        for mut cmd in vec![command("/bin/sleep"), command_as_tracee("/bin/sleep")] {
+            let mut child = cmd.arg("1").spawn().unwrap();
 
-        let status = child.try_wait().unwrap();
-        assert!(status.is_none());
+            let status = child.try_wait().unwrap();
+            assert!(status.is_none());
 
-        let status = child.wait().unwrap();
-        assert!(status.success());
+            let status = child.wait().unwrap();
+            assert!(status.success());
+        }
     }
 
     #[test]
     fn test_wait_terminated() {
-        let mut child = command("/bin/sleep").arg("1").spawn().unwrap();
+        for mut cmd in vec![command("/bin/sleep"), command_as_tracee("/bin/sleep")] {
+            let mut child = cmd.arg("1").spawn().unwrap();
 
-        thread::sleep(time::Duration::from_secs(2));
-        let status = child.wait().unwrap();
-        assert!(status.success());
+            thread::sleep(time::Duration::from_secs(2));
+            let status = child.wait().unwrap();
+            assert!(status.success());
+        }
     }
 
     #[test]
     fn test_wait_timeout() {
-        let mut child = command("/bin/sleep")
-            .arg("4")
-            .wait_timeout(1)
-            .spawn()
-            .unwrap();
+        for mut cmd in vec![command("/bin/sleep"), command_as_tracee("/bin/sleep")] {
+            let mut child = cmd.arg("4").wait_timeout(1).spawn().unwrap();
 
-        thread::sleep(time::Duration::from_secs(2));
-        let status = child.wait().unwrap();
-        assert!(!status.success());
-        assert_eq!(status.code, 128 + 9);
-        assert_eq!(status.reason, "process(/bin/sleep) received signal SIGKILL");
-        assert_eq!(status.exit_code, None);
-        assert_eq!(status.rusage.unwrap().real_time.as_secs(), 1);
+            thread::sleep(time::Duration::from_secs(2));
+            let status = child.wait().unwrap();
+            assert!(!status.success());
+            assert_eq!(status.code, 128 + 9);
+            assert_eq!(status.reason, "process(/bin/sleep) received signal SIGKILL");
+            assert_eq!(status.exit_code, None);
+            assert_eq!(status.rusage.unwrap().real_time.as_secs(), 1);
+        }
     }
 
     #[test]
     fn test_wait_killed() {
-        let mut child = command("/bin/sleep").arg("1").spawn().unwrap();
+        for mut cmd in vec![command("/bin/sleep"), command_as_tracee("/bin/sleep")] {
+            let mut child = cmd.arg("1").spawn().unwrap();
 
-        child.kill().unwrap();
-        let status = child.wait().unwrap();
-        assert!(!status.success());
-        assert_eq!(status.code, 125);
-        assert_eq!(status.reason, "container received signal SIGKILL");
-        assert_eq!(status.exit_code, None);
-        assert!(status.rusage.is_none());
+            child.kill().unwrap();
+            let status = child.wait().unwrap();
+            assert!(!status.success());
+            assert_eq!(status.code, 125);
+            assert_eq!(status.reason, "container received signal SIGKILL");
+            assert_eq!(status.exit_code, None);
+            assert!(status.rusage.is_none());
+        }
     }
 
     #[test]
     fn test_wait_then_wait() {
-        let mut child = command("/bin/sleep").arg("1").spawn().unwrap();
+        for mut cmd in vec![command("/bin/sleep"), command_as_tracee("/bin/sleep")] {
+            let mut child = cmd.arg("1").spawn().unwrap();
 
-        let status = child.wait().unwrap();
-        assert!(status.success());
+            let status = child.wait().unwrap();
+            assert!(status.success());
 
-        let status = child.wait().unwrap();
-        assert!(status.success());
+            let status = child.wait().unwrap();
+            assert!(status.success());
+        }
     }
 }
